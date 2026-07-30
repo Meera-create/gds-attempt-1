1564f0c76f6e7308569385abf18dd7e2b54a1e74
Meera.create <mirunaalini.alagarajah@yahoo.co.uk>
2026-07-30 12:14:44 +0100

    basic pages

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,43 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
+// Run this code when a form is submitted to 'age-question-1'
+// the post route should be for the page you want it to go to
+
+/*
+router.post('/question-2-location', function (req, res) {
+
+  let ageVerify = req.body['age-question']
+  if (ageVerify == "yes"){
+    res.redirect('/question-2-location');
+  } else if (ageVerify ==='yes') {
+    error:true
+  } else{
+    res.redirect('/error-page')
+  }
+});
+
+
+*/
+
+
+
+router.post('/question-2-location', function (req, res){
+
+    let locationInfo  = req.body['location-choice']
+    if (!locationInfo|| locationInfo.trim() === ""){
+        event.preventdefault();
+        return res.render('question-2-location',{
+            error:true
+        })
+    } else{
+     return res.redirect('/special-requirements-3')
+    
+    }
+
+
+});
+
+
 // Add your routes here
